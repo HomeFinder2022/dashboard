eca39f3708e76de96fec3140a2c106e54b5f7df8
Darioromaobianchi <89203731+Darioromaobianchi@users.noreply.github.com>
2023-01-10 17:28:21 +0000

    dashboardada

--- HomeFinder Dashboard/assets/controller/controllerInqui.php
+++ HomeFinder Dashboard/assets/controller/controllerInqui.php
@@ -11,11 +11,14 @@ require 'PHPMailer/src/SMTP.php';
 
   class Inquilino{
 
-    function regInqui($nome, $email, $morada, $tel, $distrito, $concelho, $freguesia, $obs){
+    function regInqui($nome, $email, $morada, $tel, $distrito, $concelho, $freguesia, $obs, $nifInqui){
         global $conn; 
+
+        session_start();
+        $nifUser = $_SESSION['nif'];
   
-          $sql = "INSERT INTO inquilino (nome, email, morada, contato, iddistrito, idconcelho, idfreguesia, observacoes) 
-          VALUES('".$nome."', '".$email."', '".$morada."', '".$tel."', '".$distrito."', '".$concelho."', '".$freguesia."', '".$obs."')";
+          $sql = "INSERT INTO inquilino (nome, email, morada, contato, iddistrito, idconcelho, idfreguesia, observacoes,idnifinquilino , idproprietario) 
+          VALUES('".$nome."', '".$email."', '".$morada."', '".$tel."', '".$distrito."', '".$concelho."', '".$freguesia."', '".$obs."', '".$nifInqui."', '".$nifUser."')";
          
           $msg = "";
           
@@ -31,6 +34,7 @@ require 'PHPMailer/src/SMTP.php';
        }
 
 
+<<<<<<< Updated upstream
 
       //  function sendMail($nome, $email){
 
@@ -604,6 +608,8 @@ require 'PHPMailer/src/SMTP.php';
 
 
 
+=======
+>>>>>>> Stashed changes
 
 
 
@@ -635,8 +641,14 @@ require 'PHPMailer/src/SMTP.php';
                 $msg .= "<td>".$row['morada']."</td>";
                 $msg .= "<td>".$row['nomedistrito']."</td>";
                 $msg .= "<td>".$row['observacoes']."</td>";
+<<<<<<< Updated upstream
                 // $msg .= "<td><button type='button' class='btn btn-danger btn-sm' onclick='delInqui(".$row['id'].")'>Apagar</button>";
                 $msg .= "<td><button type='button' class='btn btn-success btn-sm' onclick='sendEmail()'>Enviar Convite</button></td>";
+=======
+                $msg .= "<td style='text-align: center; vertical-align: middle;'><button type='button' class='btn btn-info btn-sm' onclick='editInqui(".$row['id'].")'>Info</button>";
+                $msg .= "<td style='text-align: center; vertical-align: middle;'><button type='button' class='btn btn-danger btn-sm' onclick='delInqui(".$row['id'].")'>Apagar</button>";
+                $msg .= "<td style='text-align: center; vertical-align: middle;'><button type='button' class='btn btn-primary btn-sm' onclick='sendEmail(".$row['email'].")'>Enviar Convite</button></td>";
+>>>>>>> Stashed changes
                 $msg .= "</tr>";
                 }
                 
@@ -647,6 +659,152 @@ require 'PHPMailer/src/SMTP.php';
         return $msg;
     }
 
+<<<<<<< Updated upstream
+=======
+
+    
+    function sendMail($email, $nome){
+
+      $mail = new PHPMailer\PHPMailer();
+      $mail->isSMTP();
+      $mail->Host = 'smtp.gmail.com';
+      $mail->SMTPAuth = true;
+      $mail->SMTPSecure = 'tls';
+      $mail->Username = 'homefinderpt@gmail.com';
+      $mail->Password = 'kcissgousqmemhon';
+      $mail->Port = 587;
+
+      $mail->setFrom('homefinderpt@gmail.com', 'HomeFinder');
+      $mail->addReplyTo('homefinderpt@gmail.com', 'HomeFinder');
+      $mail->addAddress($email, 'Dário Bianchi');
+      // $mail->addCC('dario_bianchi_@hotmail.com', 'Cópia');
+
+      $mail->isHTML(true);
+      $mail->Subject ="Convite de José Lebre para a HomeFinder";
+      $mail->Body    = 
+
+"<html>
+<body>
+<div class='container'>
+<div style='background-color: #2FCB6A; height: 55px;'></div>
+
+<p align='center'><img src='../img/logo-HomeFinder-mini.png' alt='logo'></p>
+
+<p>Hi ".$nome.",</p>
+
+<p>I wanted to personally invite you to join me on HomeFinder, a new platform that helps people find their dream home.</p>
+
+<p>With HomeFinder, you can search for homes in your area, save your favorite listings, and connect with top real estate agents. It's the easiest way to find your perfect home!</p>
+
+<p>I think you'll love it, and I can't wait to see what you find.</p>
+
+<p>To join, just click the link below:</p>
+
+<p><a href='http://localhost/web/PROJETO FINAL/HomeFinder/fase1/registro.php'>Sign up for HomeFinder</a></p>
+
+<p>I hope to see you on HomeFinder soon!</p>
+
+<p>Best,<br>".$nome."</p>
+
+<div style='background-color: #2FCB6A; height: 55px;'></div>
+</div>
+</body>
+</html>
+";
+
+
+
+      // http://localhost/Dario/fase1/registro.php
+
+
+      $mail->AltBody = 'Para visualizar essa mensagem acesse http://http://localhost/Web/PROJETO%20FINAL/HomeFinder/fase1/index.php';
+      $mail->addAttachment('../img/logo-HomeFinder-mini.png', 'homefinder.png');
+
+
+      if(!$mail->send()) {
+        echo 'Não foi possível enviar o convite<br>';
+        echo 'Erro: ' . $mail->ErrorInfo;
+    } else {
+        echo 'Convite enviado com sucesso!';
+    }
+    }
+
+
+
+    function removerInqui($id){
+      global $conn;
+      $msg="";
+    
+      $sql = "DELETE FROM inquilino WHERE id = ".$id;
+    
+      if ($conn->query($sql) === TRUE) {
+        $msg  = "Inquilino removido com sucesso!";
+      } else {
+        $msg = "Error: " . $sql . "<br>" . $conn->error;
+      }
+    
+      $conn->close();
+    
+      return $msg;
+    
+    }
+
+    function infoInquilino($id){
+
+      global $conn;
+    
+      $sql = "SELECT *
+      FROM inquilino WHERE id =".$id;
+    
+      $nome = "";
+      $contato = "";
+      $email = "";
+    
+      $result = $conn->query($sql);
+      if ($result->num_rows > 0) {
+          // output data of each row
+          while($row = $result->fetch_assoc()) {
+              $nome = $row['nome'];
+              $contato = $row['contato'];
+              $email = $row['email'];
+              $nif = $row['idnifinquilino'];
+              $morada = $row['morada'];
+              $freguesia = $row['idfreguesia'];
+              $distrito = $row['iddistrito'];
+              $concelho = $row['idconcelho'];
+              $obs = $row['observacoes'];
+          }
+      } 
+      $conn->close();
+  
+      $res = array("nome" => $nome, "morada"=>$morada, "contato"=>$contato, "email"=>$email, "freguesia"=>$freguesia, "distrito"=>$distrito, "concelho"=>$concelho, "obs"=>$obs, "nif"=>$nif);
+      $res = json_encode($res);
+    
+      return $res;
+    }
+
+    function guardaInquilino($id, $nome, $email, $nif, $morada, $tel, $distrito, $concelho, $freguesia, $obs){
+
+      global $conn;
+    
+      $sql = "UPDATE inquilino SET nome='".$nome."', contato ='".$tel."', email ='".$email."', morada ='".$morada."', idnifinquilino ='".$nif."', idfreguesia ='".$freguesia."', iddistrito ='".$distrito."', idconcelho ='".$concelho."', observacoes ='".$obs."'
+      WHERE id=".$id;
+     
+      $msg = "";
+          
+      if ($conn->query($sql) === TRUE) {
+        $msg  = "Inquilino Editado com Sucesso!";
+      } else {
+        $msg = "Error: " . $sql . "<br>" . $conn->error;
+      }
+    
+      $conn->close();
+    
+      return $msg;
+    
+    }
+
+>>>>>>> Stashed changes
     }
 
 
--- HomeFinder Dashboard/js/inquilino.js
+++ HomeFinder Dashboard/js/inquilino.js
@@ -7,8 +7,10 @@ function registoInqui() {
     let listaConcelhos1 = $("#listaConcelhos1").val();
     let listaFreguesias1 = $("#listaFreguesias1").val();
     let obsInqui = $("#obsInqui").val();
-    
 
+    let nifInqui = $("#nifInqui").val();
+    
+    
 
     let dados = new FormData();
     dados.append("op", 1);
@@ -20,6 +22,7 @@ function registoInqui() {
     dados.append("listaConcelhos1", listaConcelhos1);
     dados.append("listaFreguesias1", listaFreguesias1);
     dados.append("obsInqui", obsInqui);
+    dados.append("nifInqui", nifInqui);
    
 
     $.ajax({
@@ -42,15 +45,25 @@ function registoInqui() {
   }
 
 
+<<<<<<< Updated upstream
   function sendEmail() {
     let emailInqui = "dario_bianchi_@hotmail.com";
     let nomeInqui = "Dario Bianchi"; 
+=======
+  function sendEmail(email) {
+    // let emailInqui = $("#email").val();
+    let nomeInqui = "Paulo Pedras"; 
+>>>>>>> Stashed changes
 
     let dados = new FormData();
     dados.append("op", 2);
     dados.append("nomeInqui", nomeInqui);
+<<<<<<< Updated upstream
     dados.append("emailInqui", emailInqui);
     
+=======
+    dados.append("emailInqui", email);
+>>>>>>> Stashed changes
 
     $.ajax({
       url: "../assets/model/modelInqui.php",
@@ -102,6 +115,125 @@ function registoInqui() {
   
   }
 
+<<<<<<< Updated upstream
+=======
+
+  function delInqui(id){
+    let dados = new FormData();
+    dados.append('op',4);
+    dados.append('id',id);
+  
+    $.ajax({
+      url: "../assets/model/modelInqui.php",
+      method: "POST",
+      data: dados,
+      cache:false,
+      processData:false,
+      contentType: false,
+      dataType: "html"
+    })
+     
+    .done(function( resposta ) {
+      sucesso(resposta);
+      tabInqui();
+    })
+     
+    .fail(function( jqXHR, textStatus ) {
+      alert( "Request failed: " + textStatus );
+    });
+  }
+
+
+  function editInqui(id){
+    let dados = new FormData();
+    dados.append('op',5);
+    dados.append('id',id);
+  
+    $.ajax({
+      url: "../assets/model/modelInqui.php",
+      method: "POST",
+      data: dados,
+      cache:false,
+      processData:false,
+      contentType: false,
+      dataType: "html"
+    })
+     
+    .done(function( resposta ) {
+      let obj = JSON.parse(resposta);
+      $('#infoInquilino').modal('show');
+
+      $('#nomeInquiEdit').val(obj.nome);
+      $('#emailInquiEdit').val(obj.email);
+      
+      $('#nifInquiEdit').val(obj.nif);
+      $('#moradaInquiEdit').val(obj.morada);
+      $('#telInquiEdit').val(obj.contato);
+      $('#listaDistritos1Edit').val(obj.distrito);
+      $('#listaConcelhos1Edit').val(obj.concelho);
+      $('#listaFreguesias1Edit').val(obj.freguesia);
+      $('#obsInquiEdit').val(obj.obs);
+
+      $('#btnInquilino').attr('onclick', 'guardaInquilino('+id+')');
+     
+  
+    })
+     
+    .fail(function( jqXHR, textStatus ) {
+      alert( "Request failed: " + textStatus );
+    });
+  
+  }
+
+  function guardaInquilino(id){
+
+    let nomeInquiEdit = $('#nomeInquiEdit').val();
+    let emailInquiEdit = $('#emailInquiEdit').val();
+    let nifInquiEdit = $('#nifInquiEdit').val();
+    let moradaInquiEdit = $('#moradaInquiEdit').val();
+    let telInquiEdit = $('#telInquiEdit').val();
+    let listaDistritos1Edit = $('#listaDistritos1Edit').val();
+    let listaConcelhos1Edit = $('#listaConcelhos1Edit').val();
+    let listaFreguesias1Edit = $('#listaFreguesias1Edit').val();
+    let obsInquiEdit = $('#obsInquiEdit').val();
+
+    let dados = new FormData();
+    dados.append('op',6);
+    dados.append('id', id);
+    dados.append('nomeInquiEdit', nomeInquiEdit);
+    dados.append('emailInquiEdit', emailInquiEdit);
+    dados.append('nifInquiEdit', nifInquiEdit);
+    dados.append('moradaInquiEdit', moradaInquiEdit);
+    dados.append('telInquiEdit', telInquiEdit);
+    dados.append('listaDistritos1Edit', listaDistritos1Edit);
+    dados.append('listaConcelhos1Edit', listaConcelhos1Edit);
+    dados.append('listaFreguesias1Edit', listaFreguesias1Edit);
+    dados.append('obsInquiEdit', obsInquiEdit);
+    
+    $.ajax({
+      url: "../assets/model/modelInqui.php",
+      method: "POST",
+      data: dados,
+      cache:false,
+      processData:false,
+      contentType: false,
+      dataType: "html"
+    })
+     
+    .done(function( resposta ) {
+      sucesso(resposta);
+      tabInqui();
+      $('#infoInquilino').modal('hide');
+  
+    })
+     
+    .fail(function( jqXHR, textStatus ) {
+      alert( "Request failed: " + textStatus );
+    });
+  
+  }
+
+>>>>>>> Stashed changes
   function sucesso(msg) {
     Swal.fire({
       position: "center",
