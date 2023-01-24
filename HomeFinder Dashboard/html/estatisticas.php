3a05b8c09b2419fe7324715207e6effd15bcd422
Darioromaobianchi <89203731+Darioromaobianchi@users.noreply.github.com>
2023-01-24 16:06:10 +0000

    update estatisticas

--- HomeFinder Dashboard/html/estatisticas.php
+++ HomeFinder Dashboard/html/estatisticas.php
@@ -5,9 +5,88 @@
           <!-- Content wrapper -->
           <div class="content-wrapper">
 <div class="container-fluid">
-          <canvas id="myChart" width="100" height="100"></canvas>
+          <canvas id="myChart" ></canvas>
+          <canvas id="myChart2" ></canvas>
           </div>
 
+
+
+          <script>
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        datasets: [{
+            label: 'Saldo ',
+            data: [500, 1500, 3500, 2000, 4500, 3500, 4000, 5000, 3500, 4500, 6000, 5500],
+            backgroundColor: 'rgba(0, 0, 255, 0.2 )',
+            borderColor: 'rgba(0, 0, 255, 1 )',
+            borderWidth: 1
+        }, {
+            label: 'Receitas',
+            data: [500, 2000, 3000, 440, 400, 200, 500, 50, 555, 545, 524, 574],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(0, 0, 0, 1)',
+            borderWidth: 1
+        }, {
+            label: 'Despesas',
+            data: [0, 500, 3200, 400, 400, 250, 435, 520, 540, 550, 585, 574],
+            backgroundColor: 'rgba(220, 20, 60, 0.2)',
+            borderColor: 'rgba(220, 20, 60, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
+
+
+
+</script>
+
+
+<script>
+var ctx = document.getElementById('myChart2').getContext('2d');
+var chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        datasets: [{
+            label: 'Número de inquilinos ',
+            data: [4, 5, 5, 7, 7, 8, 8, 9, 9, 10, 10, 10],
+            backgroundColor: 'rgba(47, 203, 106, 0.2)',
+            borderColor: 'rgba(47, 203, 106, 2)',
+            borderWidth: 1
+        }, {
+            label: 'Número de casas',
+            data: [2, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(0, 0, 0, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
+
+
+
+</script>
    
             <!-- Content -->
             <!-- <table class="table table-hover table-bordered dataTable mt-3">
@@ -38,6 +117,9 @@
             </table> -->
 
             <!-- / Content -->
+
+            
+
 
           
   </body>
